65387028edf1383b6f80fdf03c2152e50153c4b1
Majed Mouawad <m.mmajed@hotmail.com>
2021-08-12 11:07:25 +0300

    Sorting Visualizer

---
+++ SortingAlgorithms.py
@@ -0,0 +1,91 @@
+import random
+import time
+from abc import ABCMeta, abstractmethod
+
+from pygame.surfarray import array_alpha
+
+class SortingAlgorithms():
+    def __init__(self, name):
+        self.name = name
+        self.array_to_sort = random.sample(range(800), 800)
+        self.start_time = time.time()
+
+    def udpate_display(self, index1 = None , index2 = None):
+        import SortingVisualizer
+        SortingVisualizer.update_window(self, index1, index2)
+
+    def run(self):
+        self.start_time = time.time() 
+        self.algorithm()
+        time_elapsed = time.time()-self.start_time
+        return (self.array_to_sort, time_elapsed)
+
+    @abstractmethod
+    def algorithm(self):
+        raise TypeError("")
+
+
+class SelectionSort(SortingAlgorithms):
+    def __init__(self):
+        super().__init__("Selection Sort")
+
+    def algorithm(self):
+        for i in range(len(self.array_to_sort)):
+            min_index = i
+            for j in range(i+1, len(self.array_to_sort)):
+                if self.array_to_sort[min_index] > self.array_to_sort[j]:
+                    min_index = j
+
+            tmp = self.array_to_sort[i]
+            self.array_to_sort[i] = self.array_to_sort[min_index]
+            self.array_to_sort[min_index] = tmp
+            self.udpate_display(self.array_to_sort[i], self.array_to_sort[min_index])
+
+
+class InsertionSort(SortingAlgorithms):
+    def __init__(self):
+        super().__init__("Insertion Sort")
+
+    def algorithm(self):
+        for i in range(1, len(self.array_to_sort)):
+            current = self.array_to_sort[i]
+            j = i -1
+            while j >= 0 and self.array_to_sort[j] > current:
+                self.array_to_sort[j+1] = self.array_to_sort[j]
+                j -= 1
+            self.array_to_sort[j+1] = current
+            self.udpate_display(self.array_to_sort[j+1], self.array_to_sort[i])
+
+
+class MergeSort(SortingAlgorithms):
+    def __init__(self):
+        super().__init__("Merge Sort")
+
+
+    def algorithm(self, array_to_sort = []):
+        if array_to_sort == []:
+            array_to_sort = self.array_to_sort
+        if len(array_to_sort) < 2:
+            return array_to_sort
+        mid = len(array_to_sort) // 2
+        l = self.algorithm(array_to_sort[:mid])
+        r = self.algorithm(array_to_sort[mid:])
+        return self.merge(l, r)
+
+    def merge(self, left, right):
+        sorted_array = []
+        i, j = 0,0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                sorted_array.append(left[i])
+                i += 1
+            else:
+                sorted_array.append(right[j])
+                j+=1
+            self.udpate_display()
+        sorted_array+=left[i:]
+        sorted_array+=right[j:]
+        self.array_to_sort = sorted_array
+        self.udpate_display()
+        return sorted_array
+
---
+++ SortingVisualizer.py
@@ -0,0 +1,64 @@
+import SortingAlgorithms
+import pygame
+import time
+import sys
+
+algorithms = [SortingAlgorithms.SelectionSort(), SortingAlgorithms.InsertionSort(), SortingAlgorithms.MergeSort()]
+
+pygame.init()
+display = pygame.display.set_mode((1024,800))
+display.fill((0,0,0))
+
+
+def close_window():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+def keep_open(algorithm, display, time):
+    pygame.display.set_caption(f'Sorting using {algorithm.name}  Completed in: {time} seconds')
+    while True:
+        close_window()
+        pygame.display.update()
+
+def update_window(algorithm , index1 = None, index2 = None, display =display):
+    display.fill((0,0,0))
+    pygame.display.set_caption(f'Sorting using {algorithm.name}  Time: {time.time()-algorithm.start_time}')
+    dim = int(1024/len(algorithm.array_to_sort))
+    for i in range(len(algorithm.array_to_sort)):
+        stick_color = (255,255,255)
+        if index1 == algorithm.array_to_sort[i]:
+            stick_color = (0,255,0)
+        elif index2 == algorithm.array_to_sort[i]:
+            stick_color = (255,0,0)
+        
+        pygame.draw.rect(display, stick_color, (i*dim, 800, dim, -algorithm.array_to_sort[i]),1)
+    close_window()
+    pygame.display.update()
+
+
+
+def main():
+    print("""Please input the number corresponding to the sorting algorithm you wish to test:
+1-Selection Sort
+2-Insertion Sort
+3-Merge Sort
+    \n\n\n""")
+    flag = True
+    while flag == True:
+        selected_algorithm = (input("Enter the corresponding number here: "))
+        if selected_algorithm in ("1","2","3"):
+            break
+        else:
+            print("This number doesn't match with any algorithm. Please enter a corresponding number: ")
+
+    selected_algorithm = int(selected_algorithm)
+    algo = algorithms[selected_algorithm-1]
+    time = algo.run()[1]
+    keep_open(algo, display, time)
+
+if __name__ == "__main__":
+    main()
+
+
